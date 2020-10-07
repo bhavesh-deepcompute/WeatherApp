18066abaf2cd9d97e5fd6e09dd7ac53503923f5d
bhavesh357 <bkadam357@gmail.com>
2020-10-07 16:46:59 +0530

    ADDED: demo card

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,7 +1,35 @@
 import React from "react";
-import { Grid, Button, TextField } from "@material-ui/core";
+import {
+  Grid,
+  Button,
+  TextField,
+  Card,
+  CardContent,
+  Typography,
+  makeStyles
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+	maxWidth: 375,
+	
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 export default function Weather() {
+  const classes = useStyles();
+
   return (
     <Grid container className="container">
       <Grid item md={8} className="search">
@@ -14,6 +42,32 @@ export default function Weather() {
         <Button size="small" variant="contained" color="primary">
           Search
         </Button>
+      </Grid>
+      <Grid item md={8} className="cards">
+        <Card className={classes.root} variant="outlined">
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              4:02pm,Oct 7
+            </Typography>
+            <Typography variant="h5" component="h2">
+              Bengaluru, IN
+            </Typography>
+            <Typography variant="h3" component="h2">
+              29°C
+            </Typography>
+            <Typography className={classes.pos} color="textPrimary">
+              Feels like 31°C. Scattered clouds. Light breeze
+            </Typography>
+            <Typography variant="body2" component="p">
+              2.1m/s NNW 1010hPa Humidity:55% UV:13 Dew point:19°C
+              Visibility:6.0km
+            </Typography>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );
