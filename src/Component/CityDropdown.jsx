5e6fb29d59d99dc23000d69b0ef53d24b01fd050
bhavesh357 <bkadam357@gmail.com>
2020-10-07 18:33:51 +0530

    ADDED: city dropdown

---
+++ src/Component/CityDropdown.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CityDropdown(props) {
+  return (
+    <div className="city-dropdown">
+    <div>
+      <span>
+        {props.city.name},{props.city.sys.country}
+      </span>
+    </div>
+      <div>{props.city.main.temp}</div>
+      <div>{props.city.weather[0].main}</div>
+    </div>
+  );
+}
--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Grid,
   Button,
@@ -6,13 +6,14 @@ import {
   Card,
   CardContent,
   Typography,
-  makeStyles
+  makeStyles,
 } from "@material-ui/core";
+import { Autocomplete } from "@material-ui/lab";
+import CityDropdown from "./CityDropdown";
 
 const useStyles = makeStyles({
   root: {
-	maxWidth: 375,
-	
+    maxWidth: 375,
   },
   bullet: {
     display: "inline-block",
@@ -25,19 +26,196 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  city: {
+    fontSize: 15,
+    "& > span": {
+      marginRight: 10,
+      fontSize: 18,
+    },
+  },
 });
 
 export default function Weather() {
   const classes = useStyles();
+  const [cities, setCities] = useState([
+    {
+      id: 119730,
+      name: "del",
+      coord: {
+        lat: 38.7941,
+        lon: 45.0665,
+      },
+      main: {
+        temp: 289.15,
+        feels_like: 285.85,
+        temp_min: 289.15,
+        temp_max: 289.15,
+        pressure: 1023,
+        humidity: 48,
+      },
+      dt: 1602068945,
+      wind: {
+        speed: 3.1,
+        deg: 110,
+      },
+      sys: {
+        country: "IR",
+      },
+      rain: null,
+      snow: null,
+      clouds: {
+        all: 75,
+      },
+      weather: [
+        {
+          id: 803,
+          main: "Clouds",
+          description: "broken clouds",
+          icon: "04d",
+        },
+      ],
+    },
+    {
+      id: 2660522,
+      name: "Del",
+      coord: {
+        lat: 46.6391,
+        lon: 9.5672,
+      },
+      main: {
+        temp: 278.15,
+        feels_like: 273.44,
+        temp_min: 278.15,
+        temp_max: 278.15,
+        pressure: 1016,
+        humidity: 75,
+      },
+      dt: 1602069040,
+      wind: {
+        speed: 4.1,
+        deg: 30,
+      },
+      sys: {
+        country: "CH",
+      },
+      rain: null,
+      snow: null,
+      clouds: {
+        all: 75,
+      },
+      weather: [
+        {
+          id: 803,
+          main: "Clouds",
+          description: "broken clouds",
+          icon: "04d",
+        },
+      ],
+    },
+    {
+      id: 6357301,
+      name: "Del",
+      coord: {
+        lat: 43.1167,
+        lon: -8.4662,
+      },
+      main: {
+        temp: 292.2,
+        feels_like: 292.74,
+        temp_min: 291.48,
+        temp_max: 293.15,
+        pressure: 1026,
+        humidity: 77,
+      },
+      dt: 1602069130,
+      wind: {
+        speed: 1.5,
+        deg: 320,
+      },
+      sys: {
+        country: "ES",
+      },
+      rain: null,
+      snow: null,
+      clouds: {
+        all: 40,
+      },
+      weather: [
+        {
+          id: 802,
+          main: "Clouds",
+          description: "scattered clouds",
+          icon: "03d",
+        },
+      ],
+    },
+    {
+      id: 3182996,
+      name: "Del",
+      coord: {
+        lat: 45.7692,
+        lon: 7.5567,
+      },
+      main: {
+        temp: 277.61,
+        feels_like: 269.92,
+        temp_min: 262.15,
+        temp_max: 290.15,
+        pressure: 1016,
+        humidity: 36,
+      },
+      dt: 1602069234,
+      wind: {
+        speed: 6.7,
+        deg: 230,
+      },
+      sys: {
+        country: "IT",
+      },
+      rain: null,
+      snow: null,
+      clouds: {
+        all: 30,
+      },
+      weather: [
+        {
+          id: 802,
+          main: "Clouds",
+          description: "scattered clouds",
+          icon: "03d",
+        },
+      ],
+    },
+  ]);
 
   return (
     <Grid container className="container">
       <Grid item md={8} className="search">
-        <TextField
-          label="Search City"
+        <Autocomplete
+			style={{
+				width:500
+			}}
+          freeSolo
+		  size="small"
+          options={cities}
+          classes={{
+            option: classes.city,
+          }}
           fullWidth
-          size="small"
-          variant="outlined"
+          autoHighlight
+          getOptionLabel={(city) => city.name}
+          renderOption={(city) => (
+            <CityDropdown city={city} />
+          )}
+          renderInput={(params) => (
+            <TextField
+              size="small"
+              {...params}
+              label="Choose a city"
+              variant="outlined"
+              fullWidth
+            />
+          )}
         />
         <Button size="small" variant="contained" color="primary">
           Search
